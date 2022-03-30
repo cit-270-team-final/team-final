bb444e41c702d1a98fc0768a9cc18561ed4c2f40
andreaquaisie <97315192+andreaquaisie@users.noreply.github.com>
2022-03-30 14:54:30 -0600

    Created uuid

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const app = express()
 const http = require('http')
+require('dotenv').config();
 
 const { v4: uuidv4 } = require('uuid');
 uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
+
 let invalidloginAttemps = 0;
 
-
-
-const app = express()
-const fs = require('fs')
-const md5 = require('md5');
-//const port = 443;
-
-let Invalid_loginAttempts= 0;
-
 app.use(express.static('public'));
-
-// const app = express();
 app.use(bodyParser.json());
 app.get('/', (req,res)=>{
    res.send("Hello browser");
@@ -32,7 +23,9 @@ app.post('/login',(req,res) => {
        res.status(401); 
    }
    else if (req.body.userName =="aquaisie" && (req.body.password) =="P@ssw0rd"){
-       res.send("Welcome!")
+      let myuuid = uuidv4();
+      console.log('Your UUID is: ' + myuuid);
+       res.send(myuuid)
    }
    else{
        invalidloginAttemps++;
@@ -41,41 +34,6 @@ app.post('/login',(req,res) => {
        res.send("Who are you?")
    }
 })
-
-app.get('/', (req, res) => {
-    res.send("Hello Browser");
-});
-
-//...
-// try{
-// https.createServer({
-//   key: fs.readFileSync('server.key'),
-//   cert: fs.readFileSync('server.cert')
-// }, app).listen(port, () => {
-//   console.log('Listening...')
-// })} catch(error){
-//     console.log(error)
-// }
-
-
-
-app.post('/login', (req, res) =>{
-    console.log(JSON.stringify(req.body));
-    console.log("Password given " + req.body.password)
-    if(Invalid_loginAttempts>=5){
-        res.status(403);// UNAUTHORIZED
-        res.send("Max attempts reached.")
-    }
-    else if(req.body.userName == "test@test125.com" && md5(req.body.password)== "ee6bfd6c8556ddf7fd7511b4a3c14fe7"){
-        res.send("Welcome!")
-    }else{
-        res.status(403);// UNAUTHORIZED
-        res.send("Who are you?");
-        Invalid_loginAttempts +=1
-        console.log(Invalid_loginAttempts+ "invalid attempt made")
-    }
-});
-
 
 const port = 8080;
 
