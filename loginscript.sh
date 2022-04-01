b985d980f66c99bc15cfa5cf8430399f27aa90b2
faraguti <faraguti@gmail.com>
2022-04-01 13:04:33 -0600

    Delete loginscript.sh

--- loginscript.sh
+++
@@ -1,2 +0,0 @@
-curl -d "@data.json" -X POST -H "Content-Type:application/json" http://localhost:8080/login
-curl -d "@login.json" -X POST -H "Content-Type:application/json" http://localhost:8080/createUser
