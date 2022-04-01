3f58086384037200f79738cdbb19d6dd7a4e8eba
Felipe <faraguti@byui.edu>
2022-04-01 13:36:20 -0600

    loginscript for mac users

---
+++ loginscript.sh
@@ -0,0 +1,2 @@
+curl -d "@data.json" -X POST -H "Content-Type:application/json" http://localhost:8080/login
+curl -d "@login.json" -X POST -H "Content-Type:application/json" http://localhost:8080/createUser
